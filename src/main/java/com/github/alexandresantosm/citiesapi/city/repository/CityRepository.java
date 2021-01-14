24b22d79c296c48201a591457f5c73c3792e9cb6
alexandresantosm <amenezes031@gmail.com>
2021-01-14 00:32:25 -0300

    Add State repository

--- src/main/java/com/github/alexandresantosm/citiesapi/city/repository/CityRepository.java
+++ src/main/java/com/github/alexandresantosm/citiesapi/city/repository/CityRepository.java
@@ -1,2 +1,7 @@
-package com.github.alexandresantosm.citiesapi.city.repository;public class CityRepository {
+package com.github.alexandresantosm.citiesapi.city.repository;
+
+import com.github.alexandresantosm.citiesapi.city.domain.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
 }
