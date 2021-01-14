d96512cb73289bbb99ff41136e9f9f5bdbf9fdd1
alexandresantosm <amenezes031@gmail.com>
2021-01-14 00:31:57 -0300

    Add State exception

--- src/main/java/com/github/alexandresantosm/citiesapi/city/exception/CityNotFoundException.java
+++ src/main/java/com/github/alexandresantosm/citiesapi/city/exception/CityNotFoundException.java
@@ -1,2 +1,12 @@
-package com.github.alexandresantosm.citiesapi.city.exception;public class CityNotFoundException {
+package com.github.alexandresantosm.citiesapi.city.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CityNotFoundException extends Exception {
+
+    public CityNotFoundException(Long id) {
+        super("City not found with ID " + id);
+    }
 }
