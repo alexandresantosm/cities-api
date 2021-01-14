aeeb5135c239c45ade932201bfd34a9693541f28
alexandresantosm <amenezes031@gmail.com>
2021-01-14 00:33:28 -0300

    Add State service

--- src/main/java/com/github/alexandresantosm/citiesapi/city/service/CityService.java
+++ src/main/java/com/github/alexandresantosm/citiesapi/city/service/CityService.java
@@ -1,4 +1,25 @@
 package com.github.alexandresantosm.citiesapi.city.service;
 
-public class Cityservice {
+import com.github.alexandresantosm.citiesapi.city.domain.City;
+import com.github.alexandresantosm.citiesapi.city.exception.CityNotFoundException;
+import com.github.alexandresantosm.citiesapi.city.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    public Page<City> findAllCities(Pageable page) {
+        return cityRepository.findAll(page);
+    }
+
+    public City findByIdCity(Long id) throws CityNotFoundException {
+        return cityRepository.findById(id)
+                .orElseThrow(() -> new CityNotFoundException(id));
+    }
 }
