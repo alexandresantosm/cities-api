539b84a7d26317d461384a15dc168bd3e482fd3d
alexandresantosm <amenezes031@gmail.com>
2021-01-13 23:46:42 -0300

    Add StateNotFoundException

--- src/main/java/com/github/alexandresantosm/citiesapi/states/exception/StateNotFoundException.java
+++ src/main/java/com/github/alexandresantosm/citiesapi/states/exception/StateNotFoundException.java
@@ -1,4 +1,8 @@
 package com.github.alexandresantosm.citiesapi.states.exception;
 
-public class StateNotFoundException {
+public class StateNotFoundException extends Exception {
+
+    public StateNotFoundException(Long id) {
+        super("State not found with ID " + id);
+    }
 }
